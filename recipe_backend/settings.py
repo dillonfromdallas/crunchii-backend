f10aed62525691b7b85a3f79593fc4046f6e53ce
Dillon Reyna <thedillonreyna@gmail.com>
2018-04-18 18:58:43 -0500

    Created initial models for recipes

--- recipe_backend/settings.py
+++ recipe_backend/settings.py
@@ -40,6 +40,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+
+    'photologue',
+    'sortedm2m',
+
+    'recipes',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Media Settings
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,75 @@
+import os
+
 from django.db import models
 
+from photologue.models import Gallery
+
+from recipe_backend.settings import MEDIA_ROOT
 # Create your models here.
+
+
+class Ingredients(models.Model):
+    VEGETABLE = "veg"
+    FRUIT = "fruit"
+    BERRY = "berry"
+    BEAN = "bean"
+    NUT = "nut"
+    PROTEIN = "protein"
+    SEED = "seed"
+    SPICE = "spice"
+    GRAIN = "grain"
+    BEVERAGE = "beverage"
+    COOKING_LIQUID = "liquid"
+
+    INGREDIENT_TYPE_CHOICES = (
+        (VEGETABLE, "Vegetable"),
+        (FRUIT, "Fruit"),
+        (BERRY, "Berry"),
+        (BEAN, "Bean"),
+        (NUT, "Nut"),
+        (PROTEIN, "Protein"),
+        (SEED, "Seed"),
+        (SPICE, "Spice"),
+        (GRAIN, "Grain"),
+        (BEVERAGE, "Beverage"),
+        (COOKING_LIQUID, "Cooking Liquid"),
+    )
+
+    ingredient_name = models.CharField(max_length=255)
+    type = models.CharField(max_length=255, choices=INGREDIENT_TYPE_CHOICES, blank=True)
+    calories = models.FloatField()
+
+
+class Steps(models.Model):
+    ingredients = models.ForeignKey(
+        'Ingredients',
+        on_delete=models.CASCADE,
+    )
+    instructions = models.TextField()
+
+
+class Recipe(models.Model):
+    BREAKFAST = "BREK"
+    LUNCH = "L"
+    DINNER = "DIN"
+    SNACK = "S"
+    DRINK = "BEV"
+    DESSERT = "DES"
+
+    RECIPE_TYPE_CHOICES = (
+        (BREAKFAST, "Breakfast"),
+        (LUNCH, "Lunch"),
+        (DINNER, "Dinner"),
+        (SNACK, "Snack"),
+        (DRINK, "Drink"),
+        (DESSERT, "Dessert")
+    )
+
+    recipe_type = models.CharField(max_length=9, choices=RECIPE_TYPE_CHOICES, blank=True)
+    images = models.OneToOneField(Gallery,
+                                  on_delete=models.CASCADE,
+                                  null=True)
+    steps = models.ForeignKey(
+        'Steps',
+        on_delete=models.CASCADE,
+    )
